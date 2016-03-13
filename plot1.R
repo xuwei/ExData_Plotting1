b99d3002f9270365df6c16a261415d576c2e9b87
xuwei <email.xuwei@gmail.com>
2016-03-13 20:16:51 +1100

    in progress , added solution for plot1 and plot2

--- plot1.R
+++ plot1.R
@@ -1,8 +1,20 @@
+# load libraries 
 library(dplyr)
 library(data.table)
+library(datasets)
+library(lubridate)
+
+# check if the data object we use for graphing exists, if not, read from data file and filter 
+# out the subset we need
 if (!exists("householdPowerConsumptionData")) {
   householdPowerConsumptionData <- fread("household_power_consumption.txt", na.strings = "?")
   householdPowerConsumptionData <- tbl_df(householdPowerConsumptionData)
-  householdPowerConsumptionData <- mutate(householdPowerConsumptionData, DateTime = as.Date(paste(Date, Time), format="%d/%m/%Y %H:%M:%S"))
-  householdPowerConsumptionData <- filter(householdPowerConsumptionData, DateTime >= as.Date("01/02/2007", "%d/%m/%Y") & DateTime < as.Date("03/02/2007", "%d/%m/%Y"))
+  householdPowerConsumptionData <- mutate(householdPowerConsumptionData, DateTime = fast_strptime(paste(Date, Time, sep = " "), format = "%d/%m/%Y %H:%M:%S"))
+  householdPowerConsumptionData <- filter(householdPowerConsumptionData, DateTime >= fast_strptime("01/02/2007 00:00:00", format = "%d/%m/%Y %H:%M:%S") & DateTime < fast_strptime("03/02/2007 00:00:00", format = "%d/%m/%Y %H:%M:%S"))
 }
+
+# build the graph 
+frame()
+hist(householdPowerConsumptionData$Global_active_power, col="red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.copy(png,file="plot1.png",width=480,height=480)
+dev.off()
--- plot2.R
+++ plot2.R
@@ -1,8 +1,20 @@
+# load libraries 
 library(dplyr)
 library(data.table)
+library(datasets)
+library(lubridate)
+
+# check if the data object we use for graphing exists, if not, read from data file and filter 
+# out the subset we need
 if (!exists("householdPowerConsumptionData")) {
   householdPowerConsumptionData <- fread("household_power_consumption.txt", na.strings = "?")
   householdPowerConsumptionData <- tbl_df(householdPowerConsumptionData)
-  householdPowerConsumptionData <- mutate(householdPowerConsumptionData, DateTime = as.Date(paste(Date, Time), format="%d/%m/%Y %H:%M:%S"))
-  householdPowerConsumptionData <- filter(householdPowerConsumptionData, DateTime >= as.Date("01/02/2007", "%d/%m/%Y") & DateTime < as.Date("03/02/2007", "%d/%m/%Y"))
+  householdPowerConsumptionData <- mutate(householdPowerConsumptionData, DateTime = fast_strptime(paste(Date, Time, sep = " "), format = "%d/%m/%Y %H:%M:%S"))
+  householdPowerConsumptionData <- filter(householdPowerConsumptionData, DateTime >= fast_strptime("01/02/2007 00:00:00", format = "%d/%m/%Y %H:%M:%S") & DateTime < fast_strptime("03/02/2007 00:00:00", format = "%d/%m/%Y %H:%M:%S"))
 }
+
+# build the graph 
+frame()
+plot(type = "o", pch = 20, cex = 0, householdPowerConsumptionData$DateTime, householdPowerConsumptionData$Global_active_power, xlab="",ylab="Global Active Power (kilowatts)")
+dev.copy(png,file="plot2.png",width=480,height=480)
+dev.off()
