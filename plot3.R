4b949fa4a1cac925922114d97ef73d38f02294af
xuwei <email.xuwei@gmail.com>
2016-03-13 20:35:58 +1100

    plot3 added

--- plot3.R
+++ plot3.R
@@ -1,8 +1,36 @@
+# load libraries 
 library(dplyr)
 library(data.table)
+library(datasets)
+library(lubridate)
+
+# check if the data object we use for graphing exists, if not, read from data file and filter 
+# out the subset we need
 if (!exists("householdPowerConsumptionData")) {
   householdPowerConsumptionData <- fread("household_power_consumption.txt", na.strings = "?")
   householdPowerConsumptionData <- tbl_df(householdPowerConsumptionData)
-  householdPowerConsumptionData <- mutate(householdPowerConsumptionData, DateTime = as.Date(paste(Date, Time), format="%d/%m/%Y %H:%M:%S"))
-  householdPowerConsumptionData <- filter(householdPowerConsumptionData, DateTime >= as.Date("01/02/2007", "%d/%m/%Y") & DateTime < as.Date("03/02/2007", "%d/%m/%Y"))
+  householdPowerConsumptionData <- mutate(householdPowerConsumptionData, DateTime = fast_strptime(paste(Date, Time, sep = " "), format = "%d/%m/%Y %H:%M:%S"))
+  householdPowerConsumptionData <- filter(householdPowerConsumptionData, DateTime >= fast_strptime("01/02/2007 00:00:00", format = "%d/%m/%Y %H:%M:%S") & DateTime < fast_strptime("03/02/2007 00:00:00", format = "%d/%m/%Y %H:%M:%S"))
 }
+
+# build the graph 
+frame()
+
+#submetering 1 plot
+plot(type = "o", pch = 20, cex = 0, col = "black", householdPowerConsumptionData$DateTime, householdPowerConsumptionData$Sub_metering_1, xlab = "", ylab = "")
+
+#submetering 2 plot
+lines(type = "o", pch = 20, cex = 0, col = "red", householdPowerConsumptionData$DateTime, householdPowerConsumptionData$Sub_metering_2, xlab = "", ylab = "")
+
+#submetering 3 plot
+lines(type = "o", pch = 20, cex = 0, col = "blue", householdPowerConsumptionData$DateTime, householdPowerConsumptionData$Sub_metering_3, xlab = "", ylab = "")
+
+#add legends
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), lwd = 2, cex = 0.8, col = c("black", "red", "blue"), pch=c(NA,NA,NA))
+
+# title for axes
+title(main="", sub="", 
+     xlab="", ylab="Energy sub metering")
+
+dev.copy(png,file="plot3.png",width=480,height=480)
+dev.off()
